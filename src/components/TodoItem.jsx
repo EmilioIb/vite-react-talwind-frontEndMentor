0788f11f29313dcc00c2e6aba5d39d2d54cbbe40
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2023-03-15 21:30:09 -0600

    Se añade drag and drop

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,34 +1,40 @@
 import IconCross from "./icons/IconCross";
 import IconCheck from "./icons/IconCheck";
+import React from "react";
 
-const TodoItem = ({ todo, removeTodo, updateTodo }) => {
-    const { id, title, completed } = todo;
+const TodoItem = React.forwardRef(
+    ({ todo, removeTodo, updateTodo, ...props }, ref) => {
+        const { id, title, completed } = todo;
 
-    return (
-        <article className="flex gap-4 border-b border-b-gray-400">
-            <button
-                className={`h-5 w-5 flex-none rounded-full border-2 ${
-                    completed
-                        ? "flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
-                        : "inline-block"
-                }`}
-                onClick={() => updateTodo(id)}
+        return (
+            <article
+                className="flex gap-4 border-b border-b-gray-400"
+                ref={ref}
+                {...props}
             >
-                {completed && <IconCheck />}
-            </button>
-            <p
-                className={`grow text-gray-600 transition-all duration-1000 dark:text-gray-300 ${
-                    completed && "line-through"
-                }`}
-                onClick={() => updateTodo(id)}
-            >
-                {title}
-            </p>
-            <button className="flex-none" onClick={() => removeTodo(id)}>
-                <IconCross />
-            </button>
-        </article>
-    );
-};
+                <button
+                    className={`h-5 w-5 flex-none rounded-full border-2 ${
+                        completed
+                            ? "flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"
+                            : "inline-block"
+                    }`}
+                    onClick={() => updateTodo(id)}
+                >
+                    {completed && <IconCheck />}
+                </button>
+                <p
+                    className={`grow text-gray-600 transition-all duration-1000 dark:text-gray-300 ${
+                        completed && "line-through"
+                    }`}
+                >
+                    {title}
+                </p>
+                <button className="flex-none" onClick={() => removeTodo(id)}>
+                    <IconCross />
+                </button>
+            </article>
+        );
+    }
+);
 
 export default TodoItem;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,37 @@
+import { Draggable, Droppable } from "@hello-pangea/dnd";
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, removeTodo, updateTodo }) => {
     return (
-        <div className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4">
-            {todos.map((todo) => (
-                <TodoItem
-                    key={todo.id}
-                    todo={todo}
-                    removeTodo={removeTodo}
-                    updateTodo={updateTodo}
-                />
-            ))}
-        </div>
+        <Droppable droppableId="todos">
+            {(dropableProvider) => (
+                <div
+                    ref={dropableProvider.innerRef}
+                    {...dropableProvider.droppableProps}
+                    className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+                >
+                    {todos.map((todo, index) => (
+                        <Draggable
+                            key={todo.id}
+                            index={index}
+                            draggableId={`${todo.id}`}
+                        >
+                            {(draggableProvider) => (
+                                <TodoItem
+                                    {...draggableProvider.draggableProps}
+                                    ref={draggableProvider.innerRef}
+                                    {...draggableProvider.dragHandleProps}
+                                    todo={todo}
+                                    removeTodo={removeTodo}
+                                    updateTodo={updateTodo}
+                                />
+                            )}
+                        </Draggable>
+                    ))}
+                    {dropableProvider.placeholder}
+                </div>
+            )}
+        </Droppable>
     );
 };
 
